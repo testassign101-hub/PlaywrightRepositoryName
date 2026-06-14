3f05cfa83ca420f47a4ff511626fbb86509b014d
Sudhir <testassign101@gmail.com>
2026-06-14 12:56:32 +0530

    commit

---
+++ JavaScriptPractice/Function.js
@@ -0,0 +1,27 @@
+// console.log('hello')
+// console.log(typeof(console))
+// //normal function
+// function sum(a,b){
+
+//     return a+b
+// }
+
+// console.log(sum)
+// //arrow function (comapact way for modern javascript)
+// const sub = (a,b) => {
+//     console.log('Hello');
+// }
+// console.log(sub(10,5)) //calling
+
+// callback function is a function which is passed as an argument to the parameter of another function.
+// //foreach loop in array not used for strings. and its higher order function.
+// let array = [10,20,30]
+// let elemen=0;
+// array.forEach((element , indexs, array) => {
+//     console.log(element , indexs, array)
+// });
+//map and for each are same but we use map when we want new array after some work.
+
+//filter we use filter method when we want to filter values from array like even or odd number
+
+//reduce when we want single value like sum, average
